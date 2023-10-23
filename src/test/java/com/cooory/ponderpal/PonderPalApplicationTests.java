cf01e84d389c178e2c05896e06e2d3a033d53051
Cory <cory1459@gmail.com>
2023-10-23 15:16:56 +0900

    [Cory] - First file upload

---
+++ src/test/java/com/cooory/ponderpal/PonderPalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cooory.ponderpal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PonderPalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
